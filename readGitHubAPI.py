1578d8d3949e4279c41ae881f67082fbd806fc41
EasonChen <ychen@wustl.edu>
2018-12-18 09:10:07 -0600

    reformat get_github_repos_and_stargazer

--- readGitHubAPI.py
+++ readGitHubAPI.py
@@ -29,23 +29,24 @@ def get_github_repos_and_stargazer(username, depth=3, cuzDepth=1, num_of_repos=3
 			res = []
 			for i in range(num_of_repos):
 				if i > len(data)-1: break
-				res.append(data[i]["id"])
+				res.append(data[i]["name"])
 		else:
 			res = {}
 			for i in range(num_of_repos):
 				if i > len(data)-1: break
 				owner = data[i]["owner"]["login"]
 				repoName = data[i]["name"]
+				res[repoName] = {}
 				stargazers = get_github_stargazer(owner,repoName)
 				if stargazers:
 					for stargazer in stargazers:
-						res[stargazer] = get_github_repos_and_stargazer(stargazer, depth, cuzDepth+1)
+						res[repoName][stargazer] = get_github_repos_and_stargazer(stargazer, depth, cuzDepth+1)
 		return res
 	else:
 		print ("%i:%s, fail to retrieve %s's repos" % (response.status_code, response.reason, username))
 		return None
 
-def get_github_stargazer(username, repoName,num_of_stargazer=3):
+def get_github_stargazer(username, repoName, num_of_stargazer=3):
 	url = "https://api.github.com/repos/%s/%s/stargazers" % (username,repoName)
 	response = requests.get(url)
 	if response.status_code == 200:
@@ -60,7 +61,7 @@ def get_github_stargazer(username, repoName,num_of_stargazer=3):
 		print ("%i:%s, fail to retrieve %s's repo %s's stargazer" % (response.status_code, response.reason, username, repoName))
 		return None
 
-username = "tensorflow"
+username = "yongtang"
 print(get_github_followers(username))
 print(get_github_repos_and_stargazer(username))
 
